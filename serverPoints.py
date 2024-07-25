8bac46dcb86457d7b4139878958b30d27bfe6a6d
Harbiinger <=>
2024-07-25 09:25:15 +0200

    test for server points

---
+++ serverPoints.py
@@ -0,0 +1,36 @@
+import minqlx
+
+_serverPoints_key = "minqlx:player:{}:serverPoints"
+
+class serverPoints(minqlx.Plugin):
+    def __init__(self):
+        self.add_hook("player_loaded", self.handle_player_loaded)
+        self.add_hook("game_end", self.handle_game_end)
+        self.add_hook("kill", self.handle_kill)
+        self.add_hook("death", self.handle_death)
+        self.add_hook("server_command", self.handle_server_command)
+
+    def handle_player_loaded(self, player):
+        serverPoints_key = _serverPoints_key.format(killer.steam_id)
+        if serverPoints_key not in self.db:
+            self.db[serverPoints_key] = 0
+
+        player.tell("You have ^2{} ^7points.".format(self.db[serverPoints_key]))
+
+    def handle_kill(self, victim, killer, data):
+        serverPoints_key = _serverPoints_key.format(killer.steam_id)
+        self.db[serverPoints_key] += 1
+        killer.tell("^2(+1 pt | kill)")
+
+    def handle_death(self, victim, killer, data):
+        serverPoints_key = _serverPoints_key.format(victim.steam_id)
+        if killer is None or killer.steam_id == victim.steam_id:
+            self.db[serverPoints_key] -= 5
+            killer.tell("^2(-5 pts | suicide)")
+
+    def handle_game_end(self, data):
+        pass #TODO 
+
+    def handle_server_command(self, player, cmd):
+        if cmd[0] == 'chat':
+            player.tell(player)
