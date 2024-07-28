20323c7f116861370a101cdd7ffbe0feca8ce936
Harbiinger <=>
2024-07-28 13:55:47 +0200

    serverPoints release

--- serverPoints.py
+++ serverPoints.py
@@ -18,19 +18,26 @@ class serverPoints(minqlx.Plugin):
         player.tell("You have ^2{} ^7points.".format(self.db[serverPoints_key]))
 
     def handle_kill(self, victim, killer, data):
-        serverPoints_key = _serverPoints_key.format(killer.steam_id)
-        self.db[serverPoints_key] += 1
-        killer.tell("^2(+1 pt | kill)")
+        if victim.steam_id != killer.steam_id:
+            serverPoints_key = _serverPoints_key.format(killer.steam_id)
+            pts = int(self.db[serverPoints_key])
+            self.db[serverPoints_key] = pts + 1
+            killer.tell("^2(+1 pt | kill)")
 
     def handle_death(self, victim, killer, data):
-        serverPoints_key = _serverPoints_key.format(victim.steam_id)
         if killer is None or killer.steam_id == victim.steam_id:
-            self.db[serverPoints_key] -= 5
-            killer.tell("^2(-5 pts | suicide)")
+            serverPoints_key = _serverPoints_key.format(victim.steam_id)
+            pts = int(self.db[serverPoints_key])
+            if pts >= 5:
+                self.db[serverPoints_key] = pts - 5
+                killer.tell("^2(-5 pts | suicide)")
 
     def handle_game_end(self, data):
         pass #TODO 
 
     def handle_server_command(self, player, cmd):
-        if cmd[0] == 'say':
-            player.tell(player)
+        args = cmd.split()
+        if args[0] == 'chat':
+            serverPoints_key = _serverPoints_key.format(player.steam_id)
+            cmd = cmd.replace(player.name, "[{}] {}".format(self.db[serverPoints_key], player.name))
+            return cmd
